9cbaa7b1778f24b38551c6ae99027bdc50485b37
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2023-08-04 21:11:01 +0900

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Lyu Hiroyama on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
