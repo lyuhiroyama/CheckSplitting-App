c6cd10462fa22eb09127350f24057d1f04038836
Lyu.Hiroyama <lyuhiroyama@gmail.com>
2023-08-14 21:36:46 +0900

    Added a "Grand Total (Tip Included)" section.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,16 +11,21 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    let tipPercentages = [10, 15, 20, 25, 0]
     @FocusState private var amountIsFocused: Bool
+    
+    let localCurrency: FloatingPointFormatStyle<Double>.Currency = .currency(code: Locale.current.currencyCode ?? "USD")
+    
+    var checkGrandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
+    }
     
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
-        let tipSelection = Double(tipPercentage)
-        
-        let tipValue = checkAmount / 100 * tipSelection
-        let grandTotal = checkAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
+        let amountPerPerson = checkGrandTotal / peopleCount
         
         return amountPerPerson
     }
@@ -30,7 +35,7 @@ struct ContentView: View {
             Form {
                 
                 Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    TextField("Amount", value: $checkAmount, format: localCurrency)
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
                     
@@ -41,22 +46,30 @@ struct ContentView: View {
                     }
                 }
 
+                
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
+
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
                 
+                
                 Section {
-                    Text(totalPerPerson, format:
-                            .currency(code: Locale.current.currencyCode ?? "USD"))
-                    
-                    
+                    Text(checkGrandTotal, format: localCurrency)
+                } header: {
+                    Text("Grand Total (Tip Included)")
+                }
+                
+                
+                Section {
+                    Text(totalPerPerson, format: localCurrency)
+                } header: {
+                    Text("Amount per person")
                 }
             }
             .navigationBarTitle("WeSplit")
@@ -71,6 +84,7 @@ struct ContentView: View {
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
